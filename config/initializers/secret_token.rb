50cff266802d6ac0fdfb0e44b1fbe657650999c1
vinu <vinu@phoenix.(none)>
2013-08-20 07:19:42 +0530

    initial commit and user authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Survey::Application.config.secret_token = 'beedde98a2b648e056abcde1a0a5b625518af498ed98c8014eaed1a4569452c13aa29f39dbc2badda20b14480c0c16b4e22cff09a474ea8868a637dc79dcf295'
