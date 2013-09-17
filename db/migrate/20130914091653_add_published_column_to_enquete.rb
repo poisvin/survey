d7b1f6a9d795cb9aefc6682e4253b20b8f30c7fc
vinu <vinu@phoenix.(none)>
2013-09-17 22:49:27 +0530

    committing migration

--- db/migrate/20130914091653_add_published_column_to_enquete.rb
+++ db/migrate/20130914091653_add_published_column_to_enquete.rb
@@ -1,5 +1,5 @@
 class AddPublishedColumnToEnquete < ActiveRecord::Migration
   def change
-  	add_column :enquetes, :published, :boolean, :default => 1
+  	add_column :enquetes, :published, :boolean, :default => true
   end
 end
